d9c43ebdee3b372ed9948d831fb494ed69c5ca8d
pavelkondratyev <pavel-npk@yandex.ru>
2020-03-22 21:19:50 +0500

    task1

---
+++ src/ru/kondratyev/Main.java
@@ -0,0 +1,23 @@
+package ru.kondratyev;
+
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double price = 42.5;
+        int fuel = 30;
+        System.out.println ("Введите цену бензина");
+        Scanner in = new Scanner (System.in);
+        price = in.nextDouble();
+
+        System.out.println ("Введите количество бензина");
+        fuel = in.nextInt();
+
+        double total = price*fuel;
+        System.out.println ("Стоимость бензина");
+        System.out.println (total);
+    }
+}
