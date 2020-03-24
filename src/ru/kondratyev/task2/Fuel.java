eff5194d837111498ae7860197153be2aaaa1c85
pavelkondratyev <pavel-npk@yandex.ru>
2020-03-24 22:29:44 +0500

    Добавил пакет task2

--- src/ru/kondratyev/task2/Fuel.java
+++ src/ru/kondratyev/task2/Fuel.java
@@ -1,19 +1,19 @@
-package ru.kondratyev;
+package ru.kondratyev.task2;
 
 
 
 import java.util.Scanner;
 
-public class Main {
+public class Fuel {
 
     public static void main(String[] args) {
         double price = 42.5;
         int fuel = 30;
-        System.out.println ("Введите цену бензина");
-        Scanner in = new Scanner (System.in);
+        System.out.println("Введите цену бензина");
+        Scanner in = new Scanner(System.in);
         price = in.nextDouble();
 
-        System.out.println ("Введите количество бензина");
+        System.out.println("Введите количество бензина");
         fuel = in.nextInt();
 
         double total = price*fuel;
