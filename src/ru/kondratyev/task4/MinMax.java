631c4a72b03903cb762632c3cf2871c88be5ac8d
pavelkondratyev <pavel-npk@yandex.ru>
2020-03-26 23:01:38 +0500

    Добавил класс MinMax

---
+++ src/ru/kondratyev/task4/MinMax.java
@@ -0,0 +1,17 @@
+package ru.kondratyev.task4;
+
+import java.util.Scanner;
+
+public class MinMax {
+    public static void main(String[] args) {
+        int number1, number2;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Введите число 1");
+        number1 = in.nextInt();
+        System.out.println("Введите число 2");
+        number2 = in.nextInt();
+        if (number1 < number2) System.out.println(number1 + " меньше чем " + number2);
+        else System.out.println(number2 + " меньше чем " + number1);
+    }
+}
