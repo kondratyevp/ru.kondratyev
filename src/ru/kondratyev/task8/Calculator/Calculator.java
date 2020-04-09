57b97d3d9d9efa365829e10a90043ef2c70fa2e4
pavelkondratyev <pavel-npk@yandex.ru>
2020-04-09 19:15:49 +0500

    Изменил task8 Calculator.

--- src/ru/kondratyev/task8/Calculator/Calculator.java
+++ src/ru/kondratyev/task8/Calculator/Calculator.java
@@ -1,51 +1,32 @@
 package ru.kondratyev.task8.Calculator;
 
 public class Calculator {
-    private static double a;
-    private static double b;
-    private static int percent;
 
-    static void SetAB(double na, double nb, int npercent) {
-        a = na;
-        b = nb;
-        percent = npercent;
-    }
-    //double GetA() { return a; }
-    //double GetB() { return b; }
-    //int GetPercent(){return percent;}
-
-    public static void summa() {
-        double v = a + b;
-        System.out.printf("%s+%s=%s %n", a, b, v);
+    public static void summa(double a, double b) {
+        System.out.printf("%s+%s=%s %n", a, b, a + b);
     }
 
-    public static void subtract() {
-        double v = a - b;
-        System.out.printf("%s-%s=%s %n", a, b, v);
+    public static void subtract(double a, double b) {
+        System.out.printf("%s-%s=%s %n", a, b, a - b);
     }
 
-    public static void multiplication() {
-        double v = a * b;
-        System.out.printf("%s*%s=%s %n", a, b, v);
+    public static void multiplication(double a, double b) {
+        System.out.printf("%s*%s=%s %n", a, b, a * b);
     }
 
-    public static void division() {
-        double v = a / b;
-        System.out.printf("%s/%s=%s %n", a, b, v);
+    public static void division(double a, double b) {
+        System.out.printf("%s/%s=%s %n", a, b, a / b);
     }
 
-    public static void percent() {
-        double v = a / 100 * percent;
-        System.out.printf("%s процентов от %s=%s %n", percent, a, v);
+    public static void percent(double a, int percent) {
+        System.out.printf("%s процентов от %s=%s %n", percent, a, a / 100 * percent);
     }
 
     public static void main(String[] args) {
-        Calculator num = new Calculator();
-        SetAB(4, 5, 10);
-        summa();
-        subtract();
-        multiplication();
-        division();
-        percent();
+        summa(50, 10);
+        subtract(20, 10);
+        multiplication(5.5, 6);
+        division(40, 5);
+        percent(50, 25);
     }
 }
