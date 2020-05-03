0554c11426b8bb8d22f826136d4a63c036be1218
pavelkondratyev <pavel-npk@yandex.ru>
2020-05-03 19:12:52 +0500

    Изменил task15 (добавил параметр depth).

--- src/ru/kondratyev/task15/FileAndDir.java
+++ src/ru/kondratyev/task15/FileAndDir.java
@@ -2,47 +2,58 @@ package ru.kondratyev.task15;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.nio.file.Files.exists;
 
 public class FileAndDir {
 
-    static String s = "";
+    private static String getSpaces(int n) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
 
-    static File recurs(File f) {
+    static void printContent(File f, int depth) {
         if (f.isDirectory()) {
             for (File item : f.listFiles()) {
                 if (item.isDirectory()) {
-                    System.out.println(s + item.getName() + " \t folder");
-                    s += " ";
-                    recurs(item);
+                    System.out.println(getSpaces(depth) + item.getName());
+                    printContent(item, depth + 1);
                 } else {
-                    System.out.println(s + item.getName() + "\t file");
+                    System.out.println(getSpaces(depth) + item.getName());
                 }
-                //System.out.println(len);
             }
         }
-        return f;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         File dir1 = new File("D:\\NewFolder");
         File dir2 = new File("D:\\NewFolder\\Folder2");
-        File fil1 = new File("D:\\NewFolder", "file1.txt");
-        File fil2 = new File(dir1, "file2.txt");
-        File fil3 = new File(dir1, "file3.txt");
+        File file1 = new File("D:\\NewFolder", "file1.txt");
+        File file2 = new File(dir1, "file2.txt");
+        File file3 = new File(dir1, "file3.txt");
 
+        if (!exists(Paths.get("D:\\NewFolder\\path.txt"))) {
+            Path path1 = Files.createFile(Paths.get("D:\\NewFolder\\path.txt"));
+        }
         boolean b1 = dir1.mkdir();
         boolean b2 = dir2.mkdir();
         try {
-            boolean f1 = fil1.createNewFile();
-            boolean f3 = fil3.createNewFile();
+            boolean f1 = file1.createNewFile();
+            boolean f3 = file3.createNewFile();
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
-        boolean f2 = fil1.renameTo(fil2);
-        boolean f3 = fil3.delete();
+        boolean f2 = file1.renameTo(file2);
+        boolean f3 = file3.delete();
 
-        recurs(dir1);
+        printContent(dir1, 0);
 
     }
 }
