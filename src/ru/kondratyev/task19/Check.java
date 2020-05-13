3e5f846b8aeb15cf7afe538dc118d4bfa6a28095
pavelkondratyev <pavel-npk@yandex.ru>
2020-05-13 16:30:35 +0500

    Изменил task19, добавил папку resource.

--- src/ru/kondratyev/task19/Check.java
+++ src/ru/kondratyev/task19/Check.java
@@ -1,17 +1,38 @@
 package ru.kondratyev.task19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Formatter;
 import java.util.Scanner;
 
 import static java.lang.Double.parseDouble;
 
-
 public class Check {
 
-    public static void main(String args[]) {
-        try (FileReader fr = new FileReader("D:\\NewFolder\\products.txt")) {
+    private static String[] products;
+
+    public static void getScanner(String path) throws FileNotFoundException {
+        FileReader fr = new FileReader(path);
+        Scanner scanner = new Scanner(fr);
+        int i = 0;
+        products = new String[24];
+        scanner.useDelimiter("\n");
+        while (scanner.hasNext()) {
+            products[i] = ( scanner.next() );
+            i++;
+        }
+        scanner.close();
+    }
+
+
+    public static void main(String[] args) {
+        try {
+            getScanner("resource" + File.separator + "products.txt");
+        /*(FileReader fr = new FileReader("resource" + File.separator + "products.txt")) {
             Scanner scanner = new Scanner(fr);
             int i = 0;
             String[] products = new String[24];
@@ -19,19 +40,21 @@ public class Check {
             while (scanner.hasNext()) {
                 products[i] = ( scanner.next() );
                 i++;
-            }
-            scanner.close();
-            Formatter f = new Formatter("D:\\NewFolder\\out.txt");
+            }*/
+            //scanner.close();
+
+            Formatter f = new Formatter("out" + File.separator + "out.txt");
             f.format("%-20s%-10s%-15s%-10s\n", "Наименование", "Цена", "Количество", "Стоимость");
             f.flush();
             f.format("======================================================\n");
             f.flush();
-            double total = 0.0;
+            BigDecimal total = BigDecimal.valueOf(0);
             for (int y = 0; y < products.length; y += 3) {
+                BigDecimal summa = BigDecimal.valueOf(( parseDouble(products[y + 1]) ) * ( parseDouble(products[y + 2]) ));
                 f.format("%1$-20s%4$-7s%3$s%2$-15s%5$-10.2f\n",
                         products[y], products[y + 1], "x  ", products[y + 2],
-                        ( parseDouble(products[y + 1]) ) * ( parseDouble(products[y + 2]) ));
-                total = total + ( ( parseDouble(products[y + 1]) ) * ( parseDouble(products[y + 2]) ) );
+                        summa);
+                total = total.add(summa).setScale(2, RoundingMode.HALF_DOWN);
             }
             f.flush();
             f.format("======================================================\n");
@@ -43,3 +66,4 @@ public class Check {
         }
     }
 }
+
