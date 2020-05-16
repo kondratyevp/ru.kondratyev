65245b31645e677fe31470e88b65514aea864d1c
pavelkondratyev <pavel-npk@yandex.ru>
2020-05-16 23:43:41 +0500

    Изменил task18.

--- src/ru/kondratyev/task18/Encoding.java
+++ src/ru/kondratyev/task18/Encoding.java
@@ -1,8 +1,11 @@
 package ru.kondratyev.task18;
 
 import java.io.*;
-import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
+import static java.nio.charset.Charset.forName;
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class Encoding {
@@ -17,13 +20,17 @@ public class Encoding {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
-        try (BufferedReader br = new BufferedReader(new FileReader("out" + File.separator + "text.txt", UTF_8));
-             BufferedWriter bw = new BufferedWriter(new FileWriter
-                     ("out" + File.separator + "newtext.txt", Charset.forName("Windows-1251")))) {
-            String s;
-            while (( s = br.readLine() ) != null) {
-                System.out.println(s);
-                bw.write(s + "\n");
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("out" + File.separator + "text.txt"), UTF_8);
+             BufferedWriter bw = Files.newBufferedWriter(Paths.get
+                     ("out" + File.separator + "newtext.txt"), ( forName("Windows-1251") ))) {
+            char[] buffer = new char[256];
+            int c;
+            while (( c = br.read(buffer) ) > 0) {
+                if (c < 256) {
+                    buffer = Arrays.copyOf(buffer, c);
+                }
+                System.out.println(buffer);
+                bw.write(buffer);
             }
         } catch (IOException e) {
             e.printStackTrace();
