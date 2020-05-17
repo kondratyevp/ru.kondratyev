1b3824f82068a16fe056b8c3cd03f59b4458438a
pavelkondratyev <pavel-npk@yandex.ru>
2020-05-17 12:45:48 +0500

    Изменил task21.

--- src/ru/kondratyev/task21/ArrayToLeft.java
+++ src/ru/kondratyev/task21/ArrayToLeft.java
@@ -5,20 +5,19 @@ import java.util.Arrays;
 public class ArrayToLeft {
 
     public static void toLeft(int[][] arr) {
-        int i;
-        int j = 0;
-        for (i = 0; i < arr.length; i++) {
-            for (j = 1; j < arr[i].length; j++) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr[i].length; j++) {
                 arr[i][j - 1] = arr[i][j];
             }
             arr[i][arr[i].length - 1] = 0;
         }
-        System.out.println(Arrays.deepToString(arr));
+
     }
 
     public static void main(String[] args) {
         int[][] array = {{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
                 {2, 3, 4, 5, 6, 7, 8, 9, 10, 11}, {3, 4, 5, 6, 7, 8, 9, 10, 11, 12}};
         toLeft(array);
+        System.out.println(Arrays.deepToString(array));
     }
 }
--- src/ru/kondratyev/task21/Revers.java
+++ src/ru/kondratyev/task21/Revers.java
@@ -13,9 +13,13 @@ public class Revers {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
         System.out.println(Arrays.toString(array));
+        reverseArray(array);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void reverseArray(int[] array) {
         for (int i = 0; i < ( array.length / 2 ); i++) {
             swap(array, i, array.length - 1 - i);
         }
-        System.out.println(Arrays.toString(array));
     }
 }
