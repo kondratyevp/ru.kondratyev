4c5ae51056cf0b3d66a9e02e60ba3c2693dedb20
pavelkondratyev <pavel-npk@yandex.ru>
2020-06-01 23:29:19 +0500

    Изменил task22.

--- src/ru/kondratyev/task22/PersonSuperComparator.java
+++ src/ru/kondratyev/task22/PersonSuperComparator.java
@@ -6,9 +6,10 @@ public class PersonSuperComparator implements Comparator<Person> {
 
     @Override
     public int compare(Person p1, Person p2) {
-        if (!( p1.getName().equals(p2.getName()) ))
-            return p1.getName().compareTo(p2.getName());
-        return p1.getAge() - ( p2.getAge() );
+        final int i = p1.getName().compareTo(p2.getName());
+        if (i != 0) return i;
+        return p1.getAge() - p2.getAge();
     }
 }
+
 
