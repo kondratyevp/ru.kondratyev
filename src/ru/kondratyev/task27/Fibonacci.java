0e51888effc5e476c75bfc27ce771c3876c6d5c7
pavelkondratyev <pavel-npk@yandex.ru>
2020-06-05 20:41:46 +0500

    Изменил task27 (Fibonacci).

--- src/ru/kondratyev/task27/Fibonacci.java
+++ src/ru/kondratyev/task27/Fibonacci.java
@@ -2,33 +2,33 @@ package ru.kondratyev.task27;
 
 public class Fibonacci {
 
-    int prev1 = 1;
-    int next = 1;
-    int prev2 = 0;
-
-    public void fibonacci(int num) {
+    public static void fibonacci(int num) {
+        int p1 = 0;
+        int p2 = 1;
         for (int i = 0; i < num; i++) {
-            System.out.print(" " + next);
-            next = prev2 + prev1;
-            prev2 = prev1;
-            prev1 = next;
+            System.out.print(p1 + " ");
+            p2 = p2 + p1;
+            p1 = p2 - p1;
         }
     }
 
-    public int fibonacciRecurs(int prev2, int prev1, int num) {
+    public static void fibonacciRecursPrint(int num) {
+        fibonacciRecurs(0, 1, num);
+    }
+
+    private static int fibonacciRecurs(int p2, int p1, int num) {
         if (num != 0) {
-            next = prev2 + prev1;
-            System.out.print(" " + next);
-            prev1 = fibonacciRecurs(next, prev2, num - 1);
+            System.out.print(p2 + " ");
+            p2 = p2 + p1;
+            p1 = fibonacciRecurs(p2, p2 - p1, num - 1);
         }
-        return next;
+        return p2;
     }
 
     public static void main(String[] args) {
-        Fibonacci m = new Fibonacci();
-        m.fibonacci(11);
+        fibonacci(11);
         System.out.println();
-        m.fibonacciRecurs(0, 1, 11);
+        fibonacciRecursPrint(11);
     }
 }
 
