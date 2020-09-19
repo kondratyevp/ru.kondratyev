9dc3e6c269f40d5c5b8486e62ddf32c87cc29add
pavelkondratyev <pavel-npk@yandex.ru>
2020-09-20 03:32:28 +0500

    naumen

--- src/main/java/com/company/controllers/NotesList.java
+++ src/main/java/com/company/controllers/NotesList.java
@@ -1,32 +1,83 @@
 package com.company.controllers;
 
 import com.company.models.Note;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Pattern;
 
 @Controller
 public class NotesList {
 
     private static List<Note> notes = new ArrayList<>();
+    private static List<Note> allnotes = new ArrayList<>();
+    private static int id = 0;
 
-        @GetMapping("/")
-        public String index (Model model) {
-            model.addAttribute("pageTitle", "Hello, World");
-            model.addAttribute("notes", notes);
-            return "index";
+    @GetMapping("/")
+    public String index(@RequestParam(name = "q", required = false) String q,
+                        Model model) {
+        model.addAttribute("pageTitle", "Hello, World");
+        notes = allnotes;
+        if (q != null && q.length() > 0) {
+            notes = new ArrayList<>();
+            for (Note note : allnotes) {
+                boolean r = false;
+                if (note.getTitle() != null && note.getTitle().length() > 0) {
+                    r = Pattern.compile(Pattern.quote(q), Pattern.CASE_INSENSITIVE).matcher(note.getTitle()).find();
+                }
+                if (!r && note.getText() != null && note.getText().length() > 0) {
+                    r = Pattern.compile(Pattern.quote(q), Pattern.CASE_INSENSITIVE).matcher(note.getText()).find();
+                }
+                if (r) {
+                    notes.add(note);
+                }
+            }
+        } else {
+            q = "";
         }
+        model.addAttribute("notes", notes);
+        model.addAttribute("q", q);
+        return "index";
+    }
 
-    @PostMapping("/appendnew")
-    public String appendnew (@RequestParam(name="title", required=false) String title,
-                             @RequestParam(name="text", required=false) String text,
-                             Model model) {
-       notes.add(new Note(title, text));
+    @PostMapping("/delete")
+    public String delete(@RequestParam(name = "fordelete", required = false) String fordelete,
+                         Model model, RedirectAttributes attr) {
+        System.out.println(fordelete);
+        if (fordelete != null) {
+            int i = Integer.valueOf(fordelete);
+            Iterator<Note> it = allnotes.iterator();
+            while (it.hasNext()) {
+                Note tmp = it.next();
+                if (tmp.getId() == i) {
+                    it.remove();
+                }
+            }
+        }
         return "redirect:/";
     }
+
+
+        @PostMapping("/appendnew")
+        public String appendnew ( @RequestParam(name = "title", required = false) String title,
+                @RequestParam(name = "text", required = false) String text,
+                @RequestParam(name = "fordelete", required = false) List < String > fordelete,
+                Model model, RedirectAttributes attr){
+
+            if (title.length() == 0 && text.length() == 0) {
+                attr.addFlashAttribute("emptymessage", "Введите текст либо заголовок заметки");
+                return "redirect:/";
+            }
+            allnotes.add(new Note(title, text, id++));
+            return "redirect:/";
+        }
     }
--- src/main/java/com/company/models/Note.java
+++ src/main/java/com/company/models/Note.java
@@ -3,43 +3,35 @@ package com.company.models;
 public class Note {
     private String title;
     private String text;
+    private int id;
+
+    public Note(String title, String text, int id) {
+        this.title = title;
+        this.text = text;
+        this.id = id;
+    }
 
     public String getTitle() {
         return title;
-    }
-
-    public String getText() {
-        return text;
     }
 
     public void setTitle(String title) {
         this.title = title;
     }
 
+    public String getText() {
+        return text;
+    }
+
     public void setText(String text) {
         this.text = text;
     }
 
-    public Note(String title, String text) {
-        this.title = title;
-        this.text = text;
+    public int getId() {
+        return id;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Note note = (Note) o;
-
-        if (title != null ? !title.equals(note.title) : note.title != null) return false;
-        return text != null ? text.equals(note.text) : note.text == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = title != null ? title.hashCode() : 0;
-        result = 31 * result + ( text != null ? text.hashCode() : 0 );
-        return result;
+    public void setId(int id) {
+        this.id = id;
     }
 }
